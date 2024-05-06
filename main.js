0985c825f0b1f430308fe5f5a2bb8f7cc9e28504
apacz1 <112419536+apacz1@users.noreply.github.com>
2024-05-06 17:03:25 +0200

    added and linked all buttons

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import { homepage } from "./public/pages/home";
 import { menu } from "./public/pages/menu";
+import { aboutpage } from "./public/pages/about";
 
 const homepageBtn = document.querySelector("#Home");
 const menuBtn = document.querySelector("#Menu");
+const aboutBtn = document.querySelector("#About");
 
 homepageBtn.addEventListener("click", homepage);
 menuBtn.addEventListener("click", menu);
+aboutBtn.addEventListener("click", aboutpage);
 
 homepage();
